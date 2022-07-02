6cac0a274a00dea16823683fad3da160388ab4aa
sergstar <serg.staroverov@gmail.com>
2022-07-02 23:51:09 +0300

    Commit after soving final task PYTHON-8

---
+++ project_0/game_v2.py
@@ -0,0 +1,77 @@
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
+
+import numpy as np
+
+
+def random_predict(number: int = 1) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+
+    while True:
+        count += 1
+        predict_number = np.random.randint(1, 101)  # предполагаемое число
+        if number == predict_number:
+            break  # выход из цикла если угадали
+    return count
+
+
+def random_predict_fast(number: int=np.random.randint(1, 101)) -> int:
+    """Совершенствуем алгоритм угадывания
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to random(1, 101).
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    lst_num = list(range(1, 101))
+
+    while True:
+        count += 1
+        predict_number = round(np.mean(lst_num))  # предполагаемое число
+        half = round(len(lst_num)/2)
+        if number == predict_number:
+            break  # выход из цикла если угадали
+        elif predict_number > number:
+            lst_num = lst_num[:half]
+        else:
+            lst_num = lst_num[half:]
+    return count
+
+
+
+def score_game(random_predict) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+
+
+if __name__ == "__main__":
+    # RUN
+    score_game(random_predict)
+    score_game(random_predict_fast)

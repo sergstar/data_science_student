b15c32f2216b44e18707184f5e6048680993e6b0
sergstar <serg.staroverov@gmail.com>
2022-07-02 16:54:09 +0300

    Initial commit again

--- game_v2.py
+++ game_v2.py
@@ -18,7 +18,7 @@ def random_predict(number: int = 1) -> int:
 
     while True:
         count += 1
-        predict_number = np.random.randint(2, 500)
+        predict_number = np.random.randint(1, 101)
         if number == predict_number:
             break
 
@@ -37,7 +37,7 @@ def score_game(random_predict) -> int:
 
     count_ls = []
     np.random.seed(1)
-    random_array = np.random.randint(2, 500, size=(1000))
+    random_array = np.random.randint(1, 101, size=(1000))
 
     for number in random_array:
         count_ls.append(random_predict(number))
@@ -50,21 +50,3 @@ def score_game(random_predict) -> int:
 
 if __name__ == '__main__':
     score_game(random_predict)
-
-"""print(f'Count of tries is {random_predict()}')
-
-number = np.random.randint(1, 101)
-count = 0
-
-while True:
-    count += 1
-    predict_number = int(input('Guess the number from 1 to 100'))
-
-    if predict_number > number:
-        print('Number is smaller')
-    elif predict_number < number:
-        print("Number is bigger")
-    else:
-        print(f'Your have guessed the number. This is {number} for {count} tries')
-        break
-"""
